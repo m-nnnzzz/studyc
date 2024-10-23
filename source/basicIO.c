4686ff94dc11c2bded1db448cb6212f6b82ae355
m-nnnzzz <quinine_piste0t@icloud.com>
2024-10-23 16:56:07 +0200

    IO with array *missing files

---
+++ source/basicIO.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    //create a variable
+    int age;
+
+    //message asking for input
+    printf("what is your age?\n");
+
+    //input the variable to assigned variable
+    scanf("%d", &age);
+
+    //output age
+    printf("your age is %d\n", age);
+
+    return 0;
+}
